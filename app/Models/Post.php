5d688b256a5055511824ed2587a3c40869427eba
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-02-10 17:17:02 +0200

    Add  post and author relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,14 @@ class Post extends Model
 {
     use HasFactory;
 
+    /**
+     * Relationship with the Author of the post.
+     * 
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
 }
